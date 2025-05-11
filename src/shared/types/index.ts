000b897ce65e8036a32fca2fbc4ef1d173dc64a2
Владислав Медведевский <miedviedievskii@mail.ru>
2025-05-12 02:37:32 +0500

    done

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -3,3 +3,4 @@ export * from './rental-offer.type.js';
 export * from './housing-conveniences.enum.js';
 export * from './coordinates.type.js';
 export * from './mock-server-data.type.js';
+export * from './component.enum.js';

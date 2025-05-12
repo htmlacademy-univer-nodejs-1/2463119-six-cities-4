9e422ca25f817935ad51358cf40b3465be05839e
Владислав Медведевский <miedviedievskii@mail.ru>
2025-05-12 23:06:55 +0500

    fix

--- src/shared/modules/rent-offer/rent-offer.controller.ts
+++ src/shared/modules/rent-offer/rent-offer.controller.ts
@@ -14,7 +14,7 @@ export class RentOfferController extends BaseController {
   constructor(
     @inject(Component.Logger) protected readonly logger: Logger,
     @inject(Component.RentOfferService)
-    private readonly rentOfferService: RentOfferService // @inject(Component.CommentService) private readonly commentService: CommentService,
+    private readonly rentOfferService: RentOfferService
   ) {
     super(logger);
     this.logger.info('Register routes for RentOfferController…');
@@ -25,12 +25,6 @@ export class RentOfferController extends BaseController {
       method: HttpMethod.Get,
       handler: this.getOffers,
     });
-    // this.addRoute({path: '/:id', method: HttpMethod.Post, handler: this.getOffer});
-    // this.addRoute({path: '/:id', method: HttpMethod.Patch, handler: this.update});
-    // this.addRoute({path: '/:id', method: HttpMethod.Delete, handler: this.delete});
-    // this.addRoute({path: '/:id/comments', method: HttpMethod.Get, handler: this.getComments});
-    // this.addRoute({path: '/:id/comments', method: HttpMethod.Post, handler: this.addComment});
-    // this.addRoute({path: 'city/:city/premium', method: HttpMethod.Get, handler: this.getCityPremium});
   }
 
   public async create(
@@ -45,46 +39,4 @@ export class RentOfferController extends BaseController {
     const rentOffers = await this.rentOfferService.find();
     this.ok(res, fillDTO(RentOfferResponseRdo, rentOffers));
   }
-
-  // public async getOffer(
-  //   {params}: Request<ParamOfferId>,
-  //   res: Response,
-  // ): Promise<void> {
-  //   const {rentOfferId} = params;
-  //   const result = await this.rentOfferService.findById(rentOfferId);
-  //   this.ok(res, fillDTO(RentOfferResponseRdo, result));
-  // }
-
-  // public async update({
-  //   body,
-  //   params
-  // }: UpdateRentOfferRequest, res: Response): Promise<void> {
-  //   const updatedOffer = await this.rentOfferService.patch(params.offerId, body);
-  //   this.created(res, fillDTO(RentOfferRdo, updatedOffer));
-  // }
-
-  // public async delete({params}: Request<ParamOfferId>, res: Response): Promise<void> {
-  //   const {offerId} = params;
-  //   const offer = await this.rentOfferService.delete(offerId);
-  //   this.noContent(res, offer);
-  // }
-
-  // public async getComments(
-  //   {params}: Request<ParamOfferId, UnknownRecord, UnknownRecord>,
-  //   res: Response
-  // ): Promise<void> {
-  //   const comments = await this.commentService.findByRentOfferId(params.offerId);
-  //   this.ok(res, fillDTO(CommentRdo, comments));
-  // }
-  //
-  // public async addComment({body}: CreateRentOfferRequest,
-  //   res: Response): Promise<void> {
-  //   const comment = await this.commentService.create(body);
-  //   this.created(res, fillDTO(CommentRdo, comment));
-  // }
-
-  // public async getCityPremium({params}: Request<ParamsCity>, res: Response): Promise<void> {
-  //   const offers = await this.rentOfferService.findPremiumByCity(params.city);
-  //   this.ok(res, fillDTO(RentOfferResponseRdo, offers));
-  // }
 }
